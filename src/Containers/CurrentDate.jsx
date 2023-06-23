ad76a8a27299d20ce3c5791cb52f4ddaff969043
balaji1610 <balajiece1610@gmail.com>
2023-06-23 11:45:32 +0530

    CurrentDate

---
+++ src/Containers/CurrentDate.jsx
@@ -0,0 +1,20 @@
+export default function CurrentDate() {
+  const date = new Date();
+
+  const CurrentData = `${date.getDate()}`;
+
+  const options1 = {
+    month: "long",
+  };
+  const CurrentMonth = new Intl.DateTimeFormat("en-GB", options1).format(date);
+  const CurrentYear = `${date.getFullYear()}`;
+  return (
+    <div>
+      <h2 style={{ color: "#000000" }}>
+        {CurrentData}
+        <sup>th</sup>,&nbsp;
+        {CurrentMonth},&nbsp;{CurrentYear}
+      </h2>
+    </div>
+  );
+}
--- src/Containers/Header.jsx
+++ src/Containers/Header.jsx
@@ -1,9 +1,38 @@
-import { Grid } from "@mui/material";
-
+import Grid from "@mui/material/Grid";
+import CurrentDate from "./CurrentDate";
 export default function Header() {
   return (
     <div>
-      <div className="header_layout"></div>
+      <Grid
+        container
+        columns={12}
+        spacing={5}
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        style={{
+          height: "200px",
+        }}
+      >
+        <Grid item xs={3}>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "stretch",
+            }}
+          >
+            <CurrentDate />
+          </div>
+        </Grid>
+        <Grid item xs={6}>
+          <div className="itemsGrid">xs=8</div>
+        </Grid>
+        <div></div>
+        <Grid item xs={3}>
+          <div className="itemsGrid">xs=2</div>
+        </Grid>
+      </Grid>
     </div>
   );
 }
