36d03a75a3c9678e918a547da73133149c071355
balaji1610 <balajiece1610@gmail.com>
2023-06-24 13:25:08 +0530

    Discount_Card_layout

--- src/Pages/DiscountCalculatorPage.jsx
+++ src/Pages/DiscountCalculatorPage.jsx
@@ -1,25 +1,114 @@
 import CardDiscount from "../Containers/CardDiscount";
 import Grid from "@mui/material/Grid";
+import { Button } from "@mui/material";
 export default function DiscountCalculatorPage() {
   return (
     <div>
       <CardDiscount>
-        <Grid container columns={6} rowSpacing={40} columnSpacing={20}>
+        <Grid
+          container
+          direction="row"
+          justifyContent="flex-start"
+          alignItems="center"
+          style={{ height: "20vh" }}
+        >
+          <Grid item xs={1}></Grid>
           <Grid item xs={3}>
-            <div>01</div>
+            <div>
+              <input></input>
+            </div>
           </Grid>
-          <Grid item xs={3}>
-            <div>02</div>
+          <Grid item xs={2}></Grid>
+          <Grid item xs={2}>
+            <div>
+              {" "}
+              <input></input>
+            </div>
           </Grid>
+          <Grid item xs={1}></Grid>
+        </Grid>
+        {/* Savingslayout */}
+
+        <Grid
+          container
+          direction="row"
+          justifyContent="flex-start"
+          alignItems="center"
+          style={{ height: "12vh" }}
+        >
+          <Grid item xs={2}></Grid>
+
+          <Grid
+            container
+            xs={8}
+            style={{ border: "3px solid red", heigth: "10px" }}
+          >
+            <Grid item xs={2}></Grid>
+            <Grid item xs={2}>
+              <div>Savings</div>
+            </Grid>
+            <Grid item xs={2}></Grid>
+            <Grid item xs={2}>
+              <div>Rupees</div>
+            </Grid>
+          </Grid>
+
+          <Grid item xs={2}></Grid>
         </Grid>
 
-        <Grid container columns={6} rowSpacing={40} columnSpacing={20}>
+        <Grid
+          container
+          direction="row"
+          justifyContent="flex-start"
+          alignItems="center"
+          style={{ height: "5vh" }}
+        >
+          <Grid item xs={2}></Grid>
+
+          <Grid
+            container
+            xs={8}
+            style={{
+              border: "3px solid red",
+            }}
+          >
+            <Grid item xs={2}></Grid>
+            <Grid item xs={2}>
+              <div>Savings</div>
+            </Grid>
+            <Grid item xs={2}></Grid>
+            <Grid item xs={2}>
+              <div>Rupees</div>
+            </Grid>
+          </Grid>
+
+          <Grid item xs={2}></Grid>
+        </Grid>
+
+        <div
+          style={{
+            textAlign: "center",
+            marginTop: "20px",
+            marginBottom: "20px",
+          }}
+        >
+          FIve thousand Rupess Only
+        </div>
+
+        <Grid container>
+          <Grid item xs={3}></Grid>
           <Grid item xs={3}>
-            <div>03</div>
+            <div>
+              <Button variant="contained">Submit</Button>
+            </div>
           </Grid>
           <Grid item xs={3}>
-            <div>04</div>
+            <div>
+              {" "}
+              <Button variant="contained">Clear</Button>
+            </div>
           </Grid>
+          <Grid item xs={3}></Grid>
         </Grid>
       </CardDiscount>
     </div>
